a2314d94ee15c6874c474f98097167a6f36e81f3
Therakar <matteo.pau200@gmail.com>
2022-11-08 16:37:18 +0100

    problema selezione

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ for (let i = 0; i < posts.length; i++){
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                <img class="profile-pic" src="${posts.author.image}" alt="Phil Mangione">                    
             </div>
             <div class="post-meta__data">
                 <div class="post-meta__author">Phil Mangione</div>
